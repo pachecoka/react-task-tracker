f3d1f4e5cddd2913ae7b20336828a9d1638b8e10
Karolina Pacheco <karolina_cris@hotmail.com>
2021-07-19 22:03:29 -0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import About from "./components/About";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
@@ -33,7 +36,7 @@ function App() {
       headers: {
         "Content-type": "application/json",
       },
-      body: JSON.stringify(task)
+      body: JSON.stringify(task),
     });
 
     const newTask = await res.json();
@@ -55,7 +58,7 @@ function App() {
       headers: {
         "Content-type": "application/json",
       },
-      body: JSON.stringify(taskToToggle)
+      body: JSON.stringify(taskToToggle),
     });
 
     const data = await res.json();
@@ -68,18 +71,34 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <Header
-        onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask}
-      />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        "No Tasks"
-      )}
-    </div>
+    <Router>
+      <div className="container">
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        <Route
+          path="/"
+          exact
+          render={(props) => (
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                "No Tasks"
+              )}
+            </>
+          )}
+        />
+        <Route path="/about" component={About} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
+import { useLocation } from "react-router";
 import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
+      {location.pathname === '/' && <Button
         color={showAdd ? "black" : "green"}
         text={showAdd ? "Close" : "Add"}
         onClick={onAdd}
-      />
+      />}
     </header>
   );
 };
